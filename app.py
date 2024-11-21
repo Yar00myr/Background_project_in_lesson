114da0d410082b24fb8c4e5e5d1bc8a59b399e41
Yar00myr <ivanucaromir@gmail.com>
2024-11-21 19:07:57 +0000

    done

--- app.py
+++ app.py
@@ -40,8 +40,28 @@ def process_file(task_id: str, file_path: str):
             f.write(f"File processed. Total characters: {result}\n")
 
         tasks_status[task_id] = {"status": "completed", "result_file": result_file}
+
+    except ValueError as ve:
+
+        tasks_status[task_id] = {
+            "status": "failed",
+            "error_type": "ValueError",
+            "error_message": str(ve),
+        }
+    except FileNotFoundError as fnfe:
+
+        tasks_status[task_id] = {
+            "status": "failed",
+            "error_type": "FileNotFoundError",
+            "error_message": f"File not found: {str(fnfe)}",
+        }
     except Exception as e:
-        tasks_status[task_id] = {"status": "failed", "error": str(e)}
+
+        tasks_status[task_id] = {
+            "status": "failed",
+            "error_type": "GeneralError",
+            "error_message": str(e),
+        }
 
 
 @app.post("/upload/")
@@ -51,10 +71,13 @@ async def upload_file(file: UploadFile, background_tasks: BackgroundTasks):
         raise HTTPException(status_code=400, detail="File is too large")
 
     task_id = str(uuid.uuid4())
-    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    path_for_file = os.path.join(UPLOAD_FOLDER, file.filename)
 
-    save_file(file, file_path)
-    background_tasks.add_task(process_file, task_id, file_path)
+    save_file(file, path_for_file)
+    background_tasks.add_task(
+    process_file, 
+    task_id, path_for_file
+    )
 
     tasks_status[task_id] = {"status": "queued"}
 
@@ -62,6 +85,16 @@ async def upload_file(file: UploadFile, background_tasks: BackgroundTasks):
         "task_id": task_id,
         "message": "File uploaded successfully, processing started.",
     }
+
+
+@app.get("/status/{task_id}")
+def get_task_status(task_id: str):
+    task_info = tasks_status.get(task_id)
+
+    if task_info is None:
+        raise HTTPException(status_code=404, detail=f"Task with ID {task_id} not found")
+
+    return {"task_id": task_id, "status": task_info}
 
 
 def main():
